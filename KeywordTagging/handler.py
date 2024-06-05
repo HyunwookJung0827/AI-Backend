7bb2717eee318915700e5a534b19d72034f90860
HyunwookJung0827 <as9804@berkeley.edu>
2024-06-05 15:11:57 -0700

    Back to HTML languages since PlainText couldn't have one space separating each line of job description

--- KeywordTagging/handler.py
+++ KeywordTagging/handler.py
@@ -11,7 +11,10 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 DetectorFactory.seed = 0
 
 # Set to use to detect when to catch a word for tagging
-wordStopperSet = {' ', '.', ',', '(', ')', '{', '}', '[', ']', '-', '!', '/', ':', ';', '&', '+', '<', '>'}
+wordStopperSet = {' ', '(', ')', '{', '}', '[', ']', '-', '/', ':', ';', '&', '+', '<', '>'}
+# Set to use to detect some unnecessary ends of a word. Newly added for salary info processing
+unnecessaryEndsSet = {'.', ',', '(', ')', '{', '}', '[', ']', '-', '!', '/', ':', ';', '&', '+', '<', '>'}
+# Set to use to detect when to catch a sentence for language detection
 sentenceStopperSet = {'.', '!', '。', '<', '>'}
 
 # Detectable (immigrants-friendly) language Set 
@@ -74,7 +77,7 @@ def handler():
     jobDescription = data.get('description', '')
 
     if not jobDescription:
-        return jsonify({'error': 'No job description provided'}), 400
+        return jsonify({'error 1': 'No job description provided'}), 400
 
     try:
         # detectedLanguages: Set of detected languages (unprocessed)
@@ -91,11 +94,26 @@ def handler():
         beginningOfWord = 0
         groupToKeywordDict['languages'] = set()
 
+        salaryList = []
+        skipping = False
         for jobDescriptionIndex, char in enumerate(jobDescription):
-            if char in sentenceStopperSet and beginningOfSentence < jobDescriptionIndex:
+            # Don't process anything inside the tag
+            if char == '>':
+                skipping = False
+                beginningOfSentence = jobDescriptionIndex + 1
+            elif skipping:
+                continue
+            elif char in sentenceStopperSet and beginningOfSentence < jobDescriptionIndex:
+                if char == '<':
+                    skipping = True
                 # Detect language for each sentence
                 sentence = jobDescription[beginningOfSentence: jobDescriptionIndex].strip()
-                if len(sentence) > 7:  # Skip empty sentences
+                while sentence and sentence[-1] in unnecessaryEndsSet:
+                        sentence = sentence[:-1]
+                while sentence and sentence[0] in unnecessaryEndsSet:
+                    sentence = sentence[1:]
+                if len(sentence) >= 3:  # Skip empty sentences
+                    print(sentence)
                     lang = detect(sentence)
                     detectedLanguages.add(lang)
                 # Update beginningOfSentence
@@ -107,16 +125,31 @@ def handler():
         if 'EN' in detectedLanguages:
             for jobDescriptionIndex, char in enumerate(jobDescription):
                 if char in wordStopperSet and beginningOfWord < jobDescriptionIndex:
+                    # Process each word in lowercase
                     word = jobDescription[beginningOfWord: jobDescriptionIndex].strip().lower()
-                    if word in tagDict:
-                        group = tagDict[word][0]
-                        # Example: (word: tagDict[word][0]) is ('full': 'employmentType')
-                        # Inside groupToKeywordDict, we want to store
-                        # { 'employmentType': ['full', 'part', 'hybrid']}
-                        if group in groupToKeywordDict:
-                            groupToKeywordDict[group].add(word)
-                        else:
-                            groupToKeywordDict[group] = {word}
+                    while word and word[-1] in unnecessaryEndsSet:
+                        word = word[:-1]
+                    while word and word[0] in unnecessaryEndsSet:
+                        word = word[1:]
+                    if word:
+                        if word in tagDict:
+                            group = tagDict[word][0]
+                            # Example: (word: tagDict[word][0]) is ('full': 'employmentType')
+                            # Inside groupToKeywordDict, we want to store
+                            # { 'employmentType': ['full', 'part', 'hybrid']}
+                            if group in groupToKeywordDict:
+                                groupToKeywordDict[group].add(word)
+                            else:
+                                groupToKeywordDict[group] = {word}
+                        elif word[0] == "$":
+                            # Process the salary info when $ is detected and there are less than 2 salary info
+                            # If there are more than 2 salary info, the rest are probably tips or too complicated to process 
+                            word = word[1:].replace(',', '')
+                            wordIndex = 0
+                            while wordIndex < len(word) and word[wordIndex].isdigit():
+                                wordIndex += 1
+                            salaryList.append(word[:wordIndex])
+
                         
                     # Update beginningOfWord
                     beginningOfWord = jobDescriptionIndex + 1
@@ -133,11 +166,12 @@ def handler():
         groupToKeywordDict['languages'] += [lang for lang in detectedLanguages if lang not in groupToKeywordDict['languages']]
         
     except LangDetectException as e:
-        return jsonify({'error': f'Language detection failed: {str(e)}'}), 400
+        return jsonify({'error 3': f'Language detection failed: {str(e)}'}), 400
 
     return jsonify({
         'detected_languages': detectedLanguages,
-        'group_to_keyword_dict': groupToKeywordDict
+        'group_to_keyword_dict': groupToKeywordDict,
+        'salary': salaryList
     }), 200
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
